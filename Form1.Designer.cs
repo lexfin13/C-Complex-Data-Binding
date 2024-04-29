9074c2944d4d2b60a8c014b785f86aa25481b331
LexfinCode <82054965+lexfin13@users.noreply.github.com>
2024-04-29 15:09:02 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace KomplexVeriBaglama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKitapAdi = new System.Windows.Forms.TextBox();
+            this.txtKitapYazari = new System.Windows.Forms.TextBox();
+            this.txtKitapTuru = new System.Windows.Forms.TextBox();
+            this.listeVeri = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kitap Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(31, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kitap Yazan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(31, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kitap Türü";
+            // 
+            // txtKitapAdi
+            // 
+            this.txtKitapAdi.Location = new System.Drawing.Point(142, 76);
+            this.txtKitapAdi.Name = "txtKitapAdi";
+            this.txtKitapAdi.Size = new System.Drawing.Size(143, 20);
+            this.txtKitapAdi.TabIndex = 3;
+            // 
+            // txtKitapYazari
+            // 
+            this.txtKitapYazari.Location = new System.Drawing.Point(142, 127);
+            this.txtKitapYazari.Name = "txtKitapYazari";
+            this.txtKitapYazari.Size = new System.Drawing.Size(143, 20);
+            this.txtKitapYazari.TabIndex = 4;
+            // 
+            // txtKitapTuru
+            // 
+            this.txtKitapTuru.Location = new System.Drawing.Point(142, 178);
+            this.txtKitapTuru.Name = "txtKitapTuru";
+            this.txtKitapTuru.Size = new System.Drawing.Size(143, 20);
+            this.txtKitapTuru.TabIndex = 5;
+            // 
+            // listeVeri
+            // 
+            this.listeVeri.FormattingEnabled = true;
+            this.listeVeri.Location = new System.Drawing.Point(340, 37);
+            this.listeVeri.Name = "listeVeri";
+            this.listeVeri.Size = new System.Drawing.Size(671, 277);
+            this.listeVeri.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Kitap Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 336);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listeVeri);
+            this.Controls.Add(this.txtKitapTuru);
+            this.Controls.Add(this.txtKitapYazari);
+            this.Controls.Add(this.txtKitapAdi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKitapAdi;
+        private System.Windows.Forms.TextBox txtKitapYazari;
+        private System.Windows.Forms.TextBox txtKitapTuru;
+        private System.Windows.Forms.ListBox listeVeri;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace KomplexVeriBaglama
+{
+    public partial class Form1 : Form
+    {
+        ArrayList kaynakVeri = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var kitap = new
+            {
+                KitapAdi = txtKitapAdi.Text,
+                KitapYazari=txtKitapYazari.Text,
+                KitapTuru=txtKitapTuru.Text
+            };
+            kaynakVeri.Add(kitap);
+            listeVeri.DataSource=kaynakVeri;
+            listeVeri.DisplayMember = "";
+            
+        }
+
+       
+    }
+}
